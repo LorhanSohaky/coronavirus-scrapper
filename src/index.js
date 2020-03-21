df73e5c2922f1071a6bb128a11dbe14579cda48a
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-03-21 19:21:09 -0300

    Add araraquara parser

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const wrapper = require("./ScrapperWrapper");
 const parsers = require("./parsers");
 
 const counties = [
-  { ibge_code: '3509502', name: 'Campinas', uf: 'SP', url: 'https://covid-19.campinas.sp.gov.br/', parser: parsers.campinasParser }
+  { ibge_code: '3509502', name: 'Campinas', uf: 'SP', url: 'https://covid-19.campinas.sp.gov.br/', parser: parsers.campinasParser },
+  { ibge_code: '3503208', name: 'Araraquara', uf: 'SP', url: 'http://www.araraquara.sp.gov.br/coronavirus', parser: parsers.araraquaraSPParser }
 ]
 
 const listCounties = () => Promise.all(
--- src/parsers.js
+++ src/parsers.js
@@ -13,6 +13,22 @@ const campinasParser = (html) => {
   return obj
 }
 
+const araraquaraSPParser = (html) => {
+  const $ = cheerio.load(html)
+
+  const fields = ['suspected', 'confirmed', 'negative']
+  const obj = {}
+
+  $('h3[data-mce-style]').each(function (i) {
+    const text = $(this).children('strong').text();
+
+    obj[fields[i]] = Number(text.substring(text.lastIndexOf(" ") + 1, text.length));
+  })
+
+  return obj
+}
+
 module.exports = {
-  campinasParser
+  campinasParser,
+  araraquaraSPParser
 }
